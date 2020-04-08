4a6bbbe213280938bdfe137f1889ed5aad8fddfe
Rajeev Khati <rajeevkhati5312@gmail.com>
2020-04-08 15:25:31 +0530

    add extra hotel info in hotel schema

--- models/hotel.js
+++ models/hotel.js
@@ -2,7 +2,17 @@ const mongoose = require("mongoose");
 
 let hotelSchema = new mongoose.Schema({
 	name: String,
-	location: String
+	description: String,
+	location: String,
+	cost: {
+	  AC: Number,
+	  NonAC: Number
+	},
+	wifi: Boolean,
+	swimmingPool: Boolean,
+	laudryFacilities: Boolean,
+	rating: String,
+	createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("hotel", hotelSchema);
